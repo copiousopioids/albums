f5086245fd47a9606824dc0819c8c8685bdb7d1f
Zach Marcolesco <zach.marcolesco@gmail.com>
2017-04-25 15:29:06 -0500

    Files upload, but as plain text and server hangs

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
 var port = 3000;
 var express = require('express');
-var fileUpload = require('express-fileupload');
+// var fileUpload = require('express-fileuploasd');
+var multer = require('multer');
+var imageUpload = multer({ dest: './public/images/'});
 var app = express();
+
+
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('albums.sqlite3', function(err) {
   if(err) console.error(err);
 });
 
-app.use(fileUpload({
-  safeFileNames: true
-}));
+app.use(express.static('public'));
+
+// accept one file where the name of the form field is named albumArt
+app.post('/', imageUpload.single('albumArt'), function(req, res){
+    if(!req.file) {
+      return res.status(400).send('No files were uploaded.');
+    }
+
+    albumArt = req.file;
+
+    console.log(req.body); // form fields
+    console.log(req.file); // form files
+    res.status(204).end();
+});
+ 
+// app.post('/', imageUpload.single('albumArt'), function(req, res) {
+//   if (!req.files)
+//     return res.status(400).send('No files were uploaded.');
+//  
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+//   let albumArt = req.files.albumArt;
+//  
+//   // Use the mv() method to place the file somewhere on your server 
+//   albumArt.mv('./public/images/' + albumArt.name, function(err) {
+//     if (err)
+//       return res.status(500).send(err);
+//  
+//     res.send('File uploaded!');
+//   });
+// });
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname + "/public" } );
 });
 
-app.use(express.static('public'));
-
-app.use(fileUpload());
-
-app.post('/upload', function(req, res) {
-  if (!req.files)
-    return res.status(400).send('No files were uploaded.');
-
-  console.log(req.body.filename);
-
-  var album = req.body;
-
-  // The name of the input field is used to retrieve the uploaded file 
-  let albumArt = req.files.albumArt;
-
-  create(req, res, db);
-
-  // Use the mv() method to place the file somewhere on your server 
-  albumArt.mv('/public/images/' + album.name, function(err) {
-    if (err)
-      return res.status(500).send(err);
- 
-    res.send('File uploaded!');
-  });
-});
 
 app.listen(port, function () {
   console.log('server listening on port ' + port);
